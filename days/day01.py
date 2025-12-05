88db4b334c66bd9af37c77adea4d9bddbf7625de
Ryder Lewis <ryder.lewis@gmail.com>
2025-12-05 15:39:53 -0800

    day 1

--- days/day01.py
+++ days/day01.py
@@ -6,7 +6,50 @@ class Day01(Day):
         super().__init__(*args, **kwargs)
 
     def part1(self) -> str:
-        return "dayXX 1"
+        pos = 50
+        count = 0
+
+        for line in self.data_lines():
+            steps = int(line[1:])
+            if line[0] == 'L':
+                steps = -steps
+            pos += steps
+            pos %= 100
+            if pos == 0:
+                count += 1
+
+        return str(count)
 
     def part2(self) -> str:
-        return "dayXX 2"
+        pos = 50
+        count = 0
+
+        for line in self.data_lines():
+            steps = int(line[1:])
+            if steps == 0:
+                continue
+
+            if line[0] == 'L':
+                steps = -steps
+                if pos == 0:
+                    pos = 100
+            
+            while steps < 0:
+                if pos + steps > 0:
+                    pos += steps
+                    steps = 0
+                else:
+                    count += 1
+                    steps += pos
+                    pos = 100 if steps < 0 else 0
+
+            while steps > 0:
+                if pos + steps < 100:
+                    pos += steps
+                    steps = 0
+                else:
+                    count += 1
+                    steps -= (100 - pos)
+                    pos = 0
+
+        return str(count)
