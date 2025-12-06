d0c0e5100658a0009d7fc831bfa1b4b25b07a295
Ryder Lewis <ryder.lewis@gmail.com>
2025-12-05 18:10:56 -0800

    day 2

--- days/day02.py
+++ days/day02.py
@@ -6,7 +6,36 @@ class Day02(Day):
         super().__init__(*args, **kwargs)
 
     def part1(self) -> str:
-        return "dayXX 1"
+        s = set()
+        for start, end in self.parse_input():
+            s |= self.invalid(start, end, groups=2)
+        return str(sum(s))
 
     def part2(self) -> str:
-        return "dayXX 2"
+        s = set()
+        for start, end in self.parse_input():
+            for g in range(2, len(str(end))+1):
+                s |= self.invalid(start, end, groups=g)
+        return str(sum(s))
+
+    def parse_input(self) -> list[tuple[int, int]]:
+        ret = []
+        for x in self.data().strip().split(','):
+            a, b = x.split('-')
+            ret.append((int(a), int(b)))
+        return ret
+
+    def invalid(self, start: int, end: int, groups: int = 2) -> set[int]:
+        ret = set()
+        while start <= end:
+            if len(str(start)) % groups != 0:
+                start = int('1' + '0' * (len(str(start))))
+                continue
+
+            # split the number in even groups
+            left = str(start)[:len(str(start)) // groups]
+            invalid = int(left * groups)
+            if start <= invalid <= end:
+                ret.add(invalid)
+            start = int(str(int(left)+1) + '0' * len(left) * (groups - 1))
+        return ret
