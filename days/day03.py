a0de578b445390c055e84abf62be9aa031874f3f
Ryder Lewis <ryder.lewis@gmail.com>
2025-12-05 18:20:03 -0800

    day 3 part 1

--- days/day03.py
+++ days/day03.py
@@ -6,7 +6,19 @@ class Day03(Day):
         super().__init__(*args, **kwargs)
 
     def part1(self) -> str:
-        return "dayXX 1"
+        return str(sum([self.joltage(line) for line in self.data_lines()]))
 
     def part2(self) -> str:
         return "dayXX 2"
+
+    def joltage(self, line: str) -> int:
+        max_dig = -1
+        dig_pos = -1
+
+        for i, c in enumerate(line[:-1]):
+            dig = int(c)
+            if dig > max_dig:
+                max_dig = dig
+                dig_pos = i
+        
+        return max_dig * 10 + int(max(line[dig_pos + 1:]))
