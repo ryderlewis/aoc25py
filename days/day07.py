18b26e32296f04f60c7260d311ff8b0aba5434e4
Ryder Lewis <ryder.lewis@gmail.com>
2025-12-06 23:35:23 -0800

    day 7 part 2

--- days/day07.py
+++ days/day07.py
@@ -1,14 +1,16 @@
 from .day import Day
+from functools import lru_cache
 
 
 class Day07(Day):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self._lines = self.data_lines()
 
     def part1(self) -> str:
         beams = set()
         count = 0
-        for line in self.data_lines():
+        for line in self._lines:
             for i, c in enumerate(line):
                 if c == 'S':
                     beams.add(i)
@@ -20,4 +22,15 @@ class Day07(Day):
         return str(count)
 
     def part2(self) -> str:
-        return "dayXX 2"
+        beam_col = self._lines[0].index('S')
+        return str(self.timelines(1, beam_col))
+
+    @lru_cache(maxsize=None)
+    def timelines(self, row: int, col: int) -> int:
+        while row < len(self._lines):
+            c = self._lines[row][col]
+            if c == '^':
+                return self.timelines(row + 1, col - 1) + self.timelines(row + 1, col + 1)
+            else:
+                row += 1
+        return 1
