87c59a2d9338925e227a24a6baf68d1ae079c7a6
Ryder Lewis <ryder.lewis@gmail.com>
2025-12-06 23:27:27 -0800

    day 7 part 1

--- days/day07.py
+++ days/day07.py
@@ -6,7 +6,18 @@ class Day07(Day):
         super().__init__(*args, **kwargs)
 
     def part1(self) -> str:
-        return "dayXX 1"
+        beams = set()
+        count = 0
+        for line in self.data_lines():
+            for i, c in enumerate(line):
+                if c == 'S':
+                    beams.add(i)
+                elif c == '^' and i in beams:
+                    count += 1
+                    beams.remove(i)
+                    beams.add(i-1)
+                    beams.add(i+1)
+        return str(count)
 
     def part2(self) -> str:
         return "dayXX 2"
