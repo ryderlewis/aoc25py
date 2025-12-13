865fd6de5c1bea960c742ab573386adbec0d80e8
Ryder Lewis <ryder.lewis@gmail.com>
2025-12-13 11:54:44 -0800

    day 11 part 2

--- days/day11.py
+++ days/day11.py
@@ -17,17 +17,21 @@ class Day11(Day):
         """
         Count the number of paths from device "you" to device "out".
         """
-        return str(self.count("you", "out"))
+        return str(self.count("you", "out", dac=True, fft=True))
 
     def part2(self) -> str:
-        return str(self.count("svr", "out"))
+        return str(self.count("svr", "out", dac=False, fft=False))
     
     @lru_cache(None)
-    def count(self, start: str, end: str) -> int:
+    def count(self, start: str, end: str, dac: bool, fft: bool) -> int:
+        if start == "dac":
+            dac = True
+        if start == "fft":
+            fft = True
         if start == end:
-            return 1
+            return 1 if dac and fft else 0
         dev = self.devices[start]
-        return sum(self.count(out.name, end) for out in dev.outputs)
+        return sum(self.count(out.name, end, dac, fft) for out in dev.outputs)
 
     def _parse_devices(self) -> dict[str, Device]:
         ret = {}
