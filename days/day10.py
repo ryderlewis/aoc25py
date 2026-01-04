767d3113dbc91d57369419bcac5c6c80d7feab8d
Ryder Lewis <ryder.lewis@gmail.com>
2026-01-04 13:25:32 -0800

    very slow day 10 part 2, answer = 16978

--- days/day10.py
+++ days/day10.py
@@ -2,7 +2,8 @@ from functools import lru_cache
 from .day import Day
 from collections import deque
 from dataclasses import dataclass
-import sys
+from itertools import product
+import sympy as sp
 
 @dataclass(frozen=True)
 class Machine:
@@ -36,45 +37,75 @@ class Machine:
                     d.append((new_state, count + 1))
 
     def min_joltage_presses(self) -> int:
-        buttons = tuple(sorted(self.buttons, key=lambda x: (-len(x), x)))
-        r = self._jp_recur(self.joltage, buttons, fast=True)
-        if r > 0:
-            print(f"Fast {r}: {self.joltage}")
-        else:
-            r = self._jp_recur(self.joltage, buttons, fast=False)
-            if r < 0:
-                raise ValueError(f"{self.state} is bad")
-            print(f"Slow {r}: {self.joltage}")
-        return r
+        # Variables in order: A B C D E F G H
+        print(self)
+        vars = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')[:len(self.buttons)]
+        vars_ = sp.symbols(vars, integer=True)
+        # A, B, C, D, E, F, G, H = sp.symbols("A B C D E F G H", integer=True)
+        # vars_ = [A, B, C, D, E, F, G, H]
 
+        inputs = []
+        for i in range(len(self.joltage)):
+            inputs.append([(1 if i in b else 0) for b in self.buttons])
 
-    @lru_cache(maxsize=None)
-    def _jp_recur(self, state: tuple[int, ...], buttons: tuple[tuple[int, ...], ...], fast: bool) -> int:
-        # print(f"{state}")
-        if max(state) == 0:
-            return 0
-        min_len = None
-        for b in buttons:
-            # find number of presses possible with this button
-            press_count = min(s for i, s in enumerate(state) if i in b)
-            try_count = 1 if fast else 2
+        M = sp.Matrix(inputs)
+        b = sp.Matrix(self.joltage)
 
-            while try_count > 0 and press_count > 0:
-                try_count -= 1
-                next_state = tuple(s - (press_count if i in b else 0) for i, s in enumerate(state))
-                recur_ct = self._jp_recur(next_state, buttons, fast)
-                if recur_ct >= 0:
-                    if min_len is None or press_count + recur_ct < min_len:
-                        min_len = press_count + recur_ct
-                if try_count > 0:
-                    if press_count > 1:
-                        press_count = (press_count+1)//2
-                    else:
-                        try_count = 0
+        Aug = M.row_join(b)
+        rref_aug, pivots = Aug.rref()
 
-        if min_len is None:
-            return -1
-        return min_len
+        print("Pivot columns:", pivots)
+        print("RREF:", rref_aug)
+
+        # Free variable indices among the 8 variable columns (ignore RHS col)
+        pivot_set = set(pivots)
+        free_idxs = [i for i in range(len(vars_)) if i not in pivot_set]
+        free_vars = [vars_[i] for i in free_idxs]
+
+        print("Free variables:", free_vars)
+
+        # Solve symbolically (may return expressions involving free variables like H)
+        sol_set = sp.linsolve((M, b), vars_)
+        (sol_tuple,) = list(sol_set)
+        exprs = list(sol_tuple)
+
+        S = sp.simplify(sum(exprs))
+
+        def is_nonneg_int(val):
+            val = sp.simplify(val)
+            return val.is_integer and val >= 0
+
+        best = None
+        best_point = None
+        best_free = None
+
+        # Search range for each free variable.
+        # For your case with H free, H is small because G >= 0 forces H <= 2 (odd => 1),
+        # but we'll just search a bit wider safely.
+        print(f"RYDER: {max(self.joltage)}")
+        search_ranges = [range(0, max(self.joltage)+1) for _ in free_vars]
+
+        for ct, free_vals in enumerate(product(*search_ranges), 1):
+            subs = dict(zip(free_vars, free_vals))
+            point = [sp.simplify(e.subs(subs)) for e in exprs]
+
+            if ct % 10_000 == 0 and len(free_vars) > 2:
+                print(f"RYDER: tested {ct}")
+
+            if all(is_nonneg_int(v) for v in point):
+                s_val = sp.simplify(S.subs(subs))
+                if best is None or s_val < best:
+                    best = s_val
+                    best_point = point
+                    best_free = subs
+                    if len(free_vars) > 2:
+                        print(f"RYDER: found {best}")
+
+        # print("\nBest feasible (nonnegative integer) solution:")
+        # print("free vars:", best_free)
+        # print("A B C D E F G H =", best_point)
+        print("Min sum =", best)
+        return best
 
 class Day10(Day):
     def __init__(self, *args, **kwargs):
@@ -85,7 +116,6 @@ class Day10(Day):
         return str(sum(machine.min_light_presses() for machine in self.machines))
 
     def part2(self) -> str:
-        sys.setrecursionlimit(5000)
         return str(sum(machine.min_joltage_presses() for machine in self.machines))
 
     def parse_input(self) -> list[Machine]:
